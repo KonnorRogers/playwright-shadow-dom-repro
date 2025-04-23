97171129be74cb31fdc9251d59b789e7c92f8fe5
konnorrogers <konnor5456@gmail.com>
2025-04-23 12:23:51 -0400

    initial commit

---
+++ tests/example.spec.js
@@ -0,0 +1,28 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('getByLabel on slotted content', async ({ page }) => {
+  await page.goto('localhost:8080/');
+
+  // This fails because content comes from a <slot>
+  await expect(page.getByLabel("Checkbox With Slotted Content")).toBeVisible();
+});
+
+test('getByLabel on shadow root content', async ({ page }) => {
+  await page.goto('localhost:8080/');
+  // This passes because content lives in shadow dom.
+  await expect(page.getByLabel("Checkbox With Content In Shadow Root")).toBeVisible()
+});
+
+test("getByRole on slotted content", async ({ page }) => {
+  await page.goto('localhost:8080/');
+  // This fails because content lives in a <slot>.
+  await expect(page.getByRole("checkbox", { name: "Checkbox With Slotted Content" })).toBeVisible();
+})
+
+test("getByRole on shadow root content", async ({ page }) => {
+  await page.goto('localhost:8080/');
+  // This passes because content lives in shadow dom.
+  await expect(page.getByRole("checkbox", { name: "Checkbox With Content In Shadow Root" })).toBeVisible()
+})
+
